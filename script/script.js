70f1fb4646b937c39b58dc9a170cc516c3e4e4b7
0VitorGabriel <victoropo64@outlook.com>
2023-04-04 17:50:37 -0400

    melhorei a legibilidade do código

--- script/script.js
+++ script/script.js
@@ -1,21 +1,25 @@
-let password_length = document.querySelector('input#password_length')
+const password_length_input = document.querySelector('input#password_length')
 
-let output_password_length = document.querySelector('output')
+const password_length_output = document.querySelector('output')
 
-password_length.addEventListener('input', function show_password_length() {
-    output_password_length.innerHTML = Number(password_length.value)
+password_length_input.addEventListener('input', function show_password_length() {
+    password_length_output.innerHTML = Number(password_length_input.value)
 })
 
-let generate_password_button = document.querySelector('input#generate_password_button')
+const generate_password_button = document.querySelector('input#generate_password_button')
 
-let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const characters_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-generate_password_button.addEventListener('click', function password (event) {
+generate_password_button.addEventListener('click', function generate_password (event) {
     event.preventDefault()
 
     let random_character = ''
 
-    for (let i = 0; i < password_length.value; i++) {
-        random_character += characters.charAt(Math.floor(Math.random() * characters.length))
+    for (let i = 0; i < password_length_input.value; i++) {
+        random_character += characters_list.charAt(
+            Math.floor(
+                Math.random() * characters_list.length
+            )
+        )
     }
 })

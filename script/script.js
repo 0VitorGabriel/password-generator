44e80a927ebcf0558f1cfe5929c9020201d7e9c6
0VitorGabriel <victoropo64@outlook.com>
2023-04-04 09:31:53 -0400

    functionalidade de gerar senha aleatória

--- script/script.js
+++ script/script.js
@@ -5,3 +5,17 @@ let output_password_length = document.querySelector('output')
 password_length.addEventListener('input', function show_password_length() {
     output_password_length.innerHTML = Number(password_length.value)
 })
+
+let generate_password_button = document.querySelector('input#generate_password_button')
+
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+generate_password_button.addEventListener('click', function password (event) {
+    event.preventDefault()
+
+    let random_character = ''
+
+    for (let i = 0; i < password_length.value; i++) {
+        random_character += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+})
